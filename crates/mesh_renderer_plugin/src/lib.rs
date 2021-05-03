e1d3e730c778d43a07856502376b6ef0fcd404a5
Jonas Matser <github@jonasmatser.nl>
2021-05-03 12:41:10 +0200

    Update visibilitycomponent

--- crates/mesh_renderer_plugin/src/lib.rs
+++ crates/mesh_renderer_plugin/src/lib.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::{
-    AddAsset, Added, Assets, Entity, GlobalTransform, Handle, Or, Plugin, Query, QuerySet, Res,
-    Transform,
+    AddAsset, Added, Assets, ChangeTrackers, Entity, GlobalTransform, Handle, Or, Plugin, Query,
+    QuerySet, Res, Transform, With,
 };
 use bevy::{
     ecs::{bundle::Bundle, system::IntoSystem},
@@ -45,50 +45,92 @@ impl Plugin for MeshRendererPlugin {
 }
 
 fn update_visibility(
-    mut queries: QuerySet<(
-        Query<
-            (Entity, &Handle<Mesh>, &Transform, &mut VisibilityComponent),
-            Added<VisibilityComponent>,
-        >,
-        Query<
-            (&Handle<Mesh>, &Transform, &mut VisibilityComponent),
-            Or<(Changed<Handle<Mesh>>, Changed<Transform>)>,
-        >,
+    mut query: Query<(
+        Entity,
+        &Handle<Mesh>,
+        &Transform,
+        &mut VisibilityComponent,
+        ChangeTrackers<Handle<Mesh>>,
+        ChangeTrackers<Transform>,
     )>,
     visibility_region: Res<VisibilityRegion>,
     meshes: Res<Assets<Mesh>>,
 ) {
-    queries.q0_mut().for_each_mut(
-        |(entity, mesh_handle, transform, mut visibility_component)| {
-            let mesh = meshes.get(mesh_handle).unwrap();
+    query.for_each_mut(
+        |(
+            entity,
+            mesh_handle,
+            transform,
+            mut visibility_component,
+            change_trackers_mesh_handle,
+            change_trackers_transform,
+        )| {
+            // New VisibilityComponent
+            if visibility_component.is_added() {
+                let mesh = meshes.get(mesh_handle).unwrap();
 
-            let vertex_positions = match mesh.attribute(Mesh::ATTRIBUTE_POSITION).unwrap() {
-                VertexAttributeValues::Float3(positions) => positions,
-                _ => panic!(),
+                let cull_model = mesh_to_cull_model(mesh);
+
+                let handle =
+                    visibility_region.register_dynamic_object(EntityId::from(entity), cull_model);
+
+                handle.set_transform(transform.translation, transform.rotation, transform.scale);
+
+                // TODO
+                // handle.add_feature(MeshRenderNodeHandle)
+
+                visibility_component.handle.replace(handle);
+            } else {
+                if change_trackers_mesh_handle.is_changed() {
+                    let mesh = meshes.get(mesh_handle).unwrap();
+
+                    let cull_model = mesh_to_cull_model(mesh);
+
+                    // TODO cannot currently update CullModel
+                    // visibility_component
+                    //     .handle
+                    //     .unwrap()
+                    //     .set_cull_model(Some(cull_model));
+
+                    let handle = visibility_region
+                        .register_dynamic_object(EntityId::from(entity), cull_model);
+
+                    handle.set_transform(
+                        transform.translation,
+                        transform.rotation,
+                        transform.scale,
+                    );
+
+                    visibility_component.handle.replace(handle);
+                }
+                if change_trackers_transform.is_changed() {
+                    visibility_component.handle.as_ref().unwrap().set_transform(
+                        transform.translation,
+                        transform.rotation,
+                        transform.scale,
+                    );
+                }
             }
-            .iter()
-            .map(|&floats| floats.into())
-            .collect();
-
-            let index = match mesh.indices().unwrap() {
-                mesh::Indices::U16(u16) => PolygonSoupIndex::Indexed16(u16.clone()),
-                mesh::Indices::U32(u32) => PolygonSoupIndex::Indexed32(u32.clone()),
-            };
-
-            let handle = visibility_region.register_dynamic_object(
-                EntityId::from(entity),
-                CullModel::mesh(PolygonSoup {
-                    vertex_positions,
-                    index,
-                }),
-            );
-
-            handle.set_transform(transform.translation, transform.rotation, transform.scale);
-
-            // TODO
-            // handle.add_feature(MeshRenderNodeHandle)
-
-            visibility_component.handle.replace(handle);
         },
     );
+}
+
+fn mesh_to_cull_model(mesh: &Mesh) -> CullModel {
+    let vertex_positions = match mesh.attribute(Mesh::ATTRIBUTE_POSITION).unwrap() {
+        VertexAttributeValues::Float32x3(positions) => positions,
+        _ => panic!(),
+    }
+    .iter()
+    .map(|&floats| floats.into())
+    .collect();
+
+    let index = match mesh.indices().unwrap() {
+        mesh::Indices::U16(u16) => PolygonSoupIndex::Indexed16(u16.clone()),
+        mesh::Indices::U32(u32) => PolygonSoupIndex::Indexed32(u32.clone()),
+    };
+
+    CullModel::mesh(PolygonSoup {
+        vertex_positions,
+        index,
+    })
 }
